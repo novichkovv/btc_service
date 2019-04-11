422408e4b616b7484cc3ac1ff994b0fcae64a53e
novichkovv <novichkovv@bk.ru>
2019-04-11 19:44:32 +0800

    Initial Commit

--- protected/classes/bitcoin_class.php
+++ protected/classes/bitcoin_class.php
@@ -92,7 +92,7 @@ class bitcoin_class extends base
                         'amount' => $item['amount'] - $tx_fee,
                         'tx_id' => $item['txid'],
                     ];
-                    $amount -= $item['amount'];
+                    $amount -= ($item['amount'] - $tx_fee);
                 }
             }
         }
