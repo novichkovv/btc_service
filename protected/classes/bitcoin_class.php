ce22b016f33d4a7aaa6e3634df0397403da8f72d
novichkovv <novichkovv@bk.ru>
2019-04-11 17:14:22 +0800

    Initial Commit

--- protected/classes/bitcoin_class.php
+++ protected/classes/bitcoin_class.php
@@ -10,21 +10,21 @@ class bitcoin_class extends base
     private $client;
     public function __construct()
     {
-        require_once PROTECTED_DIR . 'easy.php';
-        $bitcoin = new Bitcoin(RPC_USER,RPC_PASSWORD,"localhost","18332");
-        $b = $bitcoin->getwalletinfo();
-        var_dump($b);exit;
-//        require PROTECTED_DIR . '/vendor/autoload.php';
-//
-//        $this->client = new Denpa\Bitcoin\Client('http://' . RPC_USER . ':' . RPC_PASSWORD . '@localhost:8332/');
-//        $block = $this->client->request('getwalletinfo');
-//        var_dump($block);exit;
-//        $driver = new \Nbobtc\Http\Driver\CurlDriver();
-//        $driver
-//            ->addCurlOption(CURLOPT_VERBOSE, true)
-//            ->addCurlOption(CURLOPT_STDERR, '/var/logs/curl.err');
-//        $this->client  = new \Nbobtc\Http\Client('http://127.0.0.1:18332');
-//        $this->client->withDriver($driver);
+//        require_once PROTECTED_DIR . 'easy.php';
+//        $bitcoin = new Bitcoin(RPC_USER,RPC_PASSWORD,"localhost","18332");
+//        $b = $bitcoin->getwalletinfo();
+//        var_dump($b);exit;
+////        require PROTECTED_DIR . '/vendor/autoload.php';
+////
+////        $this->client = new Denpa\Bitcoin\Client('http://' . RPC_USER . ':' . RPC_PASSWORD . '@localhost:8332/');
+////        $block = $this->client->request('getwalletinfo');
+////        var_dump($block);exit;
+        $driver = new \Nbobtc\Http\Driver\CurlDriver();
+        $driver
+            ->addCurlOption(CURLOPT_VERBOSE, true)
+            ->addCurlOption(CURLOPT_STDERR, '/var/logs/curl.err');
+        $this->client  = new \Nbobtc\Http\Client('http://127.0.0.1:18332');
+        $this->client->withDriver($driver);
     }
 
     private function command($method, $param = null)
