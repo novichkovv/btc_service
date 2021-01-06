fd983142961ca46620cdc00408bdcb7c66400207
novichkovv <novichkovv@bk.ru>
2021-01-06 22:07:19 +0800

    Test

--- protected/runners/transaction_runner.php
+++ protected/runners/transaction_runner.php
@@ -5,10 +5,12 @@
  * Date: 16.05.18
  * Time: 10:17
  */
-define('ROOT_DIR', str_replace('protected' . DIRECTORY_SEPARATOR . 'cron', '', __DIR__));
-define('PROJECT', 'bot');
+set_time_limit(0);
+error_reporting(E_ALL);
+define('ROOT_DIR', str_replace('protected' . DIRECTORY_SEPARATOR . 'runners', '', __DIR__) . DIRECTORY_SEPARATOR);
 require_once ROOT_DIR . '/protected/config.php';
 require_once CORE_DIR . 'autoload.php';
+require_once PROTECTED_DIR . 'vendor/autoload.php';
 $addresses = new addresses_service();
 $addresses->checkTransaction();
 sleep(10);
