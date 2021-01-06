f7dc1b85176a1479526f82cbc0900a30de0782ab
novichkovv <novichkovv@bk.ru>
2021-01-06 19:55:36 +0800

    Test

--- protected/wallet_notify.php
+++ protected/wallet_notify.php
@@ -4,3 +4,6 @@ define('ROOT_DIR', str_replace('protected' . DIRECTORY_SEPARATOR . 'cron', '', _
 require_once ROOT_DIR . '/protected/config.php';
 require_once CORE_DIR . 'autoload.php';
 staticBase::writeLog('notify', file_get_contents('php://input'));
+if(2 == $argc)    {
+    staticBase::writeLog('notify', $argv[1]);
+}
